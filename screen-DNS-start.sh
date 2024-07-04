cb9148dd4b1dda0d1b95ee89e3eb8ffd52a1ade3
Luanmundim <42242586+Luanmundim@users.noreply.github.com>
2024-07-04 10:26:10 -0300

    Delete screen-DNS-start.sh

--- screen-DNS-start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Terminate all screens with "DNS" in the name
-screen -ls | grep DNS | cut -d. -f1 | awk '{print $1}' | xargs kill
-
-screen -dmS DNS bash -c 'sudo python3 /home/scripts/DNS/DNS.py'
-
-echo "Script de divulgacao DNS foi iniciado"
