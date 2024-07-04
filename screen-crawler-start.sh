a1e8d7fa432a8f25e0211475d457f2144a143917
Luanmundim <42242586+Luanmundim@users.noreply.github.com>
2024-07-04 10:26:16 -0300

    Delete screen-crawler-start.sh

--- screen-crawler-start.sh
+++
@@ -1,8 +0,0 @@
-screen -ls | grep crawler | cut -d. -f1 | awk '{print $1}' | xargs kill
-
-screen -dmS crawler bash -c 'sudo python3 /home/scripts/crawler/crawler.py'
-echo "Script de monitoramento crawler foi iniciado!"
-
-# Kill the screen session after the script has completed
-#screen -S crawler -X quit
-echo "Script de monitoramento crawler foi finalizado!"
