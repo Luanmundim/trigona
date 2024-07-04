c6af9b536d12b097010e1591313bc46c5c4fd2e5
Luanmundim <42242586+Luanmundim@users.noreply.github.com>
2024-07-04 15:53:53 -0300

    Create runningAll.py

---
+++ runningAll.py
@@ -0,0 +1,61 @@
+import os
+import logging
+from datetime import datetime
+import socket
+from time import sleep
+import random
+
+def start_service(status, service, delay):
+    # Change directory to /home/ubuntu/trigona/service/
+    os.chdir(f'/home/ubuntu/trigona/{service}/')
+
+    # Configure logging
+    current_day = datetime.now().strftime("%Y-%m-%d")
+    hostname = socket.gethostname()
+    log_file = f'/home/ubuntu/trigona/{service}/{hostname}-{current_day}-logging-{service}.log'
+    logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    # Run ./config-service.sh start
+    os.system(f'cd /home/ubuntu/trigona/{service} && echo "./config-{service}.sh {status}" | at now + {delay} minutes')
+    logging.info(f'Started {service} with status: {status}')
+
+
+
+def start_cowrie(status, service):
+    # Change directory to /home/ubuntu/trigona/service/
+    os.chdir(f'/home/ubuntu/trigona/cowrie/{service}/')
+
+    # Configure logging
+    current_day = datetime.now().strftime("%Y-%m-%d")
+    hostname = socket.gethostname()
+    log_file = f'/home/ubuntu/trigona/{service}/{hostname}-{current_day}-logging-{service}.log'
+    logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    # Run ./config-service.sh start
+    os.system(f'cd /home/ubuntu/trigona/cowrie/{service} && ./bin/{service} {status}')
+    logging.info(f'Started {service} with status: {status}')
+
+
+def generate_number():
+    return random.randint(2, 12*58)
+
+if __name__ == '__main__':
+    start_service('start', 'tcpdump', 0)#must be the firtst one to start
+    sleep(1)
+    start_service('start', 'server', 0)
+    sleep(1)
+    start_service('start', 'serverHTTPS', 0)
+    sleep(1)
+    start_cowrie('start', 'cowrie', 0)
+    sleep(1)
+
+
+    if ('crawler' in socket.gethostname):
+        start_service('start', 'crawler', generate_number())
+        sleep(1)
+    if ('dns' in socket.gethostname):
+        start_service('start', 'DNS', generate_number())
+        sleep(1)
+    if ('request' in socket.gethostname):
+        start_service('start', 'request', generate_number())
+        sleep(1)
