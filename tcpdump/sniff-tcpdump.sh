39baaa14ac66e8970fb841f68a2af406daefabdc
Luanmundim <42242586+Luanmundim@users.noreply.github.com>
2024-07-09 16:21:48 -0300

    Update sniff-tcpdump.sh

--- tcpdump/sniff-tcpdump.sh
+++ tcpdump/sniff-tcpdump.sh
@@ -4,7 +4,7 @@
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
 
 # Ensure the directory exists
-mkdir -p "/home/scripts/log/tcpdump/"
+mkdir -p "/home/ubuntu/log/tcpdump/"
 
 # Run tcpdump and save the output
-tcpdump -i eth0 > "/home/scripts/log/tcpdump/${HOSTNAME}-${TIMESTAMP}-tcpdump.pcap"
+tcpdump -i eth0 > "/home/ubuntu/log/tcpdump/${HOSTNAME}-${TIMESTAMP}-tcpdump.pcap"
