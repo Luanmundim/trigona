501d3aa857ce374eb47ed32c05037e31038a61b0
Luanmundim <42242586+Luanmundim@users.noreply.github.com>
2024-07-12 12:18:27 -0300

    Update server.py

--- server/server.py
+++ server/server.py
@@ -5,12 +5,28 @@ from html import escape
 import json
 import os
 import traceback
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
 import http.server
 import urllib.parse as urlparse
 
 # Ensure the directory exists
 log_directory = "/home/ubuntu/log/server"
 os.makedirs(log_directory, exist_ok=True)
+
+hostname = socket.gethostname()
+current_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+log_filename = f"{log_directory}/{hostname}-server.log"
+
+# Create a rotating log handler
+log_handler = TimedRotatingFileHandler(log_filename, when="midnight", interval=1, backupCount=30)
+log_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+
+# Create a logger and add the log handler
+logger = logging.getLogger()
+logger.addHandler(log_handler)
+logger.setLevel(logging.INFO)
 
 def get_ipv6_address():
     # Get the host name
@@ -21,71 +37,6 @@ def get_ipv6_address():
         # Extract the IPv6 address
         ipv6_address = address[4][0]
         return ipv6_address
-
-
-def create_log_files():
-    hostname = socket.gethostname()
-    current_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    log_filename = f"{log_directory}/{hostname}-{current_datetime}-server.log"
-    json_filename = f"{log_directory}/{hostname}-{current_datetime}-server.json"
-    errors_filename = f"{log_directory}/{hostname}-{current_datetime}-errors.log"
-
-    if not os.path.exists(log_filename):
-        with open(log_filename, 'w') as file:
-            json.dump([], file)
-        print(f"Log file {log_filename} created.")
-
-    if not os.path.exists(json_filename):
-        with open(json_filename, 'w') as file:
-            json.dump([], file)
-        print(f"JSON file {json_filename} created.")
-    
-    if not os.path.exists(errors_filename):
-        with open(errors_filename, 'w') as file:
-            json.dump([], file)
-        print(f"Errors file {errors_filename} created.")
-
-# Call the function before using the log_attempt and write_server_log functions
-
-
-def write_server_log(condition):
-    timestamp = datetime.datetime.now()
-    log_message = {
-        "Event": f"Server {condition}",
-        "Timestamp": str(timestamp)
-    }
-
-    hostname = socket.gethostname()
-    current_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = f"{log_directory}/{hostname}-{current_datetime}-server.log"
-    with open(filename, 'r+') as file:
-        logs = json.load(file)
-        logs.append(log_message)
-        file.seek(0)
-        json.dump(logs, file, indent=4)
-        file.truncate()
-        print(f"Server log saved in {filename}")
-
-
-def log_error(error_message):
-    timestamp = datetime.datetime.now()
-    log_message = {
-        "Event": "Error",
-        "Timestamp": str(timestamp),
-        "Error Message": error_message,
-        "Line": traceback.format_exc().splitlines()[-1]
-    }
-    hostname = socket.gethostname()
-    current_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = f"{log_directory}/{hostname}-{current_datetime}-errors.log"
-    with open(filename, 'r+') as file:
-        logs = json.load(file)
-        logs.append(log_message)
-        file.seek(0)
-        json.dump(logs, file, indent=4)
-        file.truncate()
-        print(f"Error logged in {filename}")
-
 
 def log_attempt(self, username, password, method, status_code, user_agent, destinationIP):
     timestamp = datetime.datetime.now()
@@ -103,21 +54,10 @@ def log_attempt(self, username, password, method, status_code, user_agent, desti
         "Timestamp": str(timestamp)
     }  
 
-    hostname = socket.gethostname()
-    current_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-    filename = f"{log_directory}/{hostname}-{current_datetime}-server.json"
-    with open(filename, 'r+') as file:
-        logs = json.load(file)
-        logs.append(log_message)
-        file.seek(0)
-        json.dump(logs, file, indent=4)
-        file.truncate()
-        print(f"Log saved in {filename}")
-
+    logger.info(json.dumps(log_message, indent=4))
 
 class IPv6Server(socketserver.TCPServer):
     address_family = socket.AF_INET6
-
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
@@ -141,7 +81,7 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
             
             log_attempt(self, '', '', 'GET', '200', user_agent, destinationIP)
         except Exception as e:
-            log_error(str(e))
+            logger.error(str(e))
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
@@ -162,21 +102,18 @@ class MyHandler(http.server.SimpleHTTPRequestHandler):
         try:
             log_attempt(self, username, password, 'POST', status_code, user_agent, destinationIP)
         except Exception as e:
-            log_error(str(e))
-
+            logger.error(str(e))
 
 if __name__ == '__main__':
     try:
-        create_log_files()
         destinationIP = get_ipv6_address()
         server_address = ('::', 8080)
         httpd = IPv6Server(server_address, MyHandler)
         print(f'''Server running on http://[{destinationIP}]:8080''')
-        write_server_log('starts')
+        logger.info(f"Server starts at {datetime.datetime.now()}")
         httpd.serve_forever()
     except KeyboardInterrupt:
-        write_server_log('stops')
+        logger.info(f"Server stops at {datetime.datetime.now()}")
         print('Server stopped')
     except Exception as e:
-        log_error(str(e))
-
+        logger.error(str(e))
